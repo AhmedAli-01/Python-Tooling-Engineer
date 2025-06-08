0e95de816d070ad79e99edccbb58a84719b8f316
AhmedAli-01 <axmed13@hotmail.com>
2025-06-08 19:22:37 +0100

    First day with python

---
+++ greeter.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+print(f"Hello, {name}! Welcome back to Python.")
+print(f"Do you feel like {age} years old?")
---
+++ loops.py
@@ -0,0 +1,21 @@
+for i in range(3):
+    print("Hello!")
+
+
+
+def greet(name):
+    print(f"Hello, {name}!")
+    
+greet("Toil")
+
+
+names = ["Toil", "Amina", "Zaki"]
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+for person in names:
+    greet(person)
+
+for i in range(3):
+    print(i)
